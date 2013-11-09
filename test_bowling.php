7b0b37f4381785b0a346c3738a65cbcae3feafc4
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-11-08 20:23:35 -0700

    Test all gutter balls

--- test_bowling.php
+++ test_bowling.php
@@ -1,9 +1,9 @@
 <?php
 
-  class CompoundInterestTest extends PHPUnit_Framework_TestCase{
-    public function test_do_nothing()
+  class TestBowling extends PHPUnit_Framework_TestCase{
+    public function test_all_gutter_balls()
     {
-
+      $this->assertEquals(score([0]),0);
     }
   }
 
